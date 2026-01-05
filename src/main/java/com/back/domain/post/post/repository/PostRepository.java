11fdc9ca9720c93b3f90d06600ebb25f966b8b29
nam3280 <nam3280@naver.com>
2026-01-05 17:17:14 +0900

    0014

--- src/main/java/com/back/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/back/domain/post/post/repository/PostRepository.java
@@ -4,9 +4,7 @@ import com.back.domain.post.post.document.Post;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PostRepository extends ElasticsearchRepository<Post,String> {
     List<Post> findAll();
-    Optional<Post> findById(String id);
 }
--- src/main/java/com/back/global/initData/BaseInitData.java
+++ src/main/java/com/back/global/initData/BaseInitData.java
@@ -20,7 +20,6 @@ public class BaseInitData {
         return args->{
             work1();
             work2();
-            work3();
         };
     }
 
@@ -42,13 +41,6 @@ public class BaseInitData {
         log.debug("기존 Post 전체 조회");
         for (Post post : postService.findAll()) {
             log.debug("Existing Post: {}", post);
-        }
-    }
-
-    private void work3() {
-        log.debug("Post 단건 조회");
-        for (Post post : postService.findAll()) {
-            log.debug("조회된 Post: {}", post);
         }
     }
 }

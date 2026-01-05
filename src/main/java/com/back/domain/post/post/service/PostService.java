4debb34235aeb1db5ea77ecedccd1927d33ad9d0
nam3280 <nam3280@naver.com>
2026-01-05 16:44:46 +0900

    0012

--- src/main/java/com/back/domain/post/post/service/PostService.java
+++ src/main/java/com/back/domain/post/post/service/PostService.java
@@ -5,6 +5,8 @@ import com.back.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class PostService {
@@ -18,5 +20,9 @@ public class PostService {
     public Post create(String title, String content, String author) {
         Post post = new Post(title, content, author);
         return postRepository.save(post);
+    }
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
     }
 }
